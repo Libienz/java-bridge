912e6cbbd1957bc5c70ca225d6b8b1855ba2db23
Lkh-dev <Lkh10195@gmail.com>
2023-12-07 13:44:56 +0900

    feat: 입력을 도메인으로 매핑하는 클래스 InputMapper 작성

---
+++ src/main/java/bridge/InputMapper.java
@@ -0,0 +1,24 @@
+package bridge;
+
+import bridge.domain.Direction;
+import bridge.domain.GameCommand;
+
+public class InputMapper {
+    private static final String NON_NUMERIC_VALUE_MESSAGE = "[ERROR] 숫자로 된 값이 아닙니다";
+
+    public int mapToInt(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NON_NUMERIC_VALUE_MESSAGE);
+        }
+    }
+
+    public Direction mapToDirection(String input) {
+        return Direction.from(input);
+    }
+
+    public GameCommand mapToGameCommand(String input) {
+        return GameCommand.from(input);
+    }
+}
