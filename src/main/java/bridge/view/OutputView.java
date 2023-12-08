bd2634770c8a347078409cc84f563a85ebf34211
libienz <Leekh10195@gmail.com>
2023-12-08 11:38:00 +0900

    feat: OutputView 구현

--- src/main/java/bridge/view/OutputView.java
+++ src/main/java/bridge/view/OutputView.java
@@ -1,16 +1,29 @@
 package bridge.view;
 
+import bridge.dto.GameResultDto;
+import bridge.dto.RoundResultsDto;
+
 /**
  * 사용자에게 게임 진행 상황과 결과를 출력하는 역할을 한다.
  */
 public class OutputView {
+    private static final String GREETING_MESSAGE = "다리 건너기 게임을 시작합니다.";
+    private final OutputMessageResolver outputMessageResolver;
 
+    public OutputView(OutputMessageResolver outputMessageResolver) {
+        this.outputMessageResolver = outputMessageResolver;
+    }
+
+    public void printGreeting() {
+        System.out.println(GREETING_MESSAGE);
+    }
     /**
      * 현재까지 이동한 다리의 상태를 정해진 형식에 맞춰 출력한다.
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printMap() {
+    public void printMap(RoundResultsDto roundResultsDto) {
+        System.out.println(outputMessageResolver.resolveRoundResultMessage(roundResultsDto));
     }
 
     /**
@@ -18,6 +31,7 @@ public class OutputView {
      * <p>
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void printResult() {
+    public void printResult(GameResultDto gameResultDto) {
+        System.out.println(outputMessageResolver.resolveGameResultMessage(gameResultDto));
     }
 }
